e8e500d0c4e4f24d88c0f4e20092012369e35f86
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-11-05 14:33:46 +0530

    updated sol

--- SQL/SQL50/SQL10.sql
+++ SQL/SQL50/SQL10.sql
@@ -3,11 +3,14 @@
 
 -- https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
 
-select 
-a.machine_id,
-round(
-      (select avg(a1.timestamp) from Activity a1 where a1.activity_type = 'end' and a1.machine_id = a.machine_id) - 
-      (select avg(a1.timestamp) from Activity a1 where a1.activity_type = 'start' and a1.machine_id = a.machine_id)
-,3) as processing_time
-from Activity a
-group by a.machine_id
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) processing_time
+from Activity a1 JOIN Activity a2
+where 
+a1.machine_id = a2.machine_id
+AND 
+a1.process_id = a2.process_id
+AND 
+a1.activity_type="start"
+AND 
+a2.activity_type="end"
+group by a1.machine_id
