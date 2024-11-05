b899034e35c7122d79aeead8b2f7b22456286854
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-11-05 12:51:47 +0530

    updated better solution

--- SQL/SQL50/SQL8.sql
+++ SQL/SQL50/SQL8.sql
@@ -3,23 +3,8 @@
 
 -- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
 
--- Solution 1
-SELECT 
-    v.customer_id, 
-    COUNT(v.visit_id) AS count_no_trans
-FROM 
-    Visits v
-LEFT JOIN 
-    Transactions t 
-ON 
-    v.visit_id = t.visit_id  
-WHERE 
-    t.transaction_id IS NULL
-GROUP BY 
-    v.customer_id;
-
--- Solution 2
-select customer_id , count(visit_id) as count_no_trans from Visits
-where 
-visit_id not in (select  visit_id from Transactions)
-group by customer_id;
+select customer_id, count(v.visit_id) as count_no_trans
+from Visits v LEFT JOIN Transactions t
+using(visit_id)
+where t.visit_id IS NULL -- or use t.transaction_id IS NULL
+group by customer_id
