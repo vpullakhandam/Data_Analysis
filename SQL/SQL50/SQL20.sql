2f3d98530e27ac4f6a11693fb695002006a622a7
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-11-08 20:26:34 +0530

    updated sol

---
+++ SQL/SQL50/SQL20.sql
@@ -0,0 +1,16 @@
+-- Monthly Transactions I
+
+-- https://leetcode.com/problems/monthly-transactions-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+SELECT 
+    LEFT(trans_date, 7) AS month,
+    country, 
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    month, country;
