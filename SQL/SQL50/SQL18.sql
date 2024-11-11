8eeaf82b30631a7e985394ec1ee32eff583ea453
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-11-11 20:02:10 +0530

    updated solution to a question

--- SQL/SQL50/SQL18.sql
+++ SQL/SQL50/SQL18.sql
@@ -3,8 +3,10 @@
 
 -- https://leetcode.com/problems/percentage-of-users-attended-a-contest/description/?envType=study-plan-v2&envId=top-sql-50
 
+
 select contest_id, round(count(DISTINCT r.user_id)*100/(select count(*) from Users),2) as percentage
 from Users as u left join Register as r
 using (user_id)
+where contest_id IS NOT NULL
 group by contest_id
 order by percentage DESC, contest_id
